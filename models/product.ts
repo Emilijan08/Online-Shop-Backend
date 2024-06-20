b7d238296c2465ed34503dcfb58c7c2876dc0610
Jonathan Russ <jonathan.russ@bluewin.ch>
2024-06-20 22:06:32 +0200

    Update product model

--- models/product.ts
+++ models/product.ts
@@ -17,9 +17,9 @@ interface IProduct extends Document {
 }
 
 const commentSchema = new Schema<IComment>({
-  text: { type: String, required: true },
-  user: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now }
+  text: { type: String, required: false },
+  user: { type: String, required: false },
+  createdAt: { type: Date, default: Date.now, required: false }
 })
 
 const productSchema = new Schema<IProduct>({
@@ -27,9 +27,9 @@ const productSchema = new Schema<IProduct>({
   productImage: { type: String, required: true },
   price: { type: Number, required: true },
   brandName: { type: String, required: true },
-  gender: { type: String, required: true },
-  type: { type: String, required: true },
-  comments: [commentSchema]
+  gender: { type: String, required: false },
+  type: { type: String, required: false },
+  comments: [commentSchema, { required: false }]
 })
 
 export const Product = model<IProduct>('Product', productSchema)
